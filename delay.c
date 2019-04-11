9a4d952e1415827f49217ac4985013efafe495bd
4497cv <4497cv@gmail.com>
2019-04-10 21:17:18 -0500

    added other necessary files for system operation and and main file

---
+++ delay.c
@@ -0,0 +1,10 @@
+#include <delay.h>
+
+void delay(uint16_t delay)
+{
+	volatile uint16_t counter;
+
+	for(counter = delay; counter > 0; counter--)
+	{
+	}
+}
---
+++ main.c
@@ -0,0 +1,26 @@
+
+/**
+ * @file    RTC_Project.c
+ * @brief   Application entry point.
+ */
+#include <stdio.h>
+#include "system.h"
+
+int main(void) {
+	uint8_t flag_hour = FALSE;
+	uint8_t mb_flag;
+	system_init();
+
+	for(;;)
+	{
+		mb_flag = UART_get_mailbox_flag(UART_0);
+		mb_mailbox = UART_get_mailbox(UART_0);
+		if(mb_flag)
+		{
+
+		}
+		UART_empty_mailbox(UART_0);
+	}
+
+    return 0 ;
+}
