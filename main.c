41ae19e39590b842d2f371aaf7a4a5431ceb2161
4497cv <4497cv@gmail.com>
2019-04-11 02:09:39 -0500

    implemented RTC and time functions

--- main.c
+++ main.c
@@ -3,11 +3,12 @@
  * @file    RTC_Project.c
  * @brief   Application entry point.
  */
-#include <stdio.h>
 #include "system.h"
 
 int main(void) {
 	uint8_t mb_flag;
+	uint8_t mb_mailbox;
+
 	system_init();
 
 	for(;;)
@@ -16,9 +17,27 @@ int main(void) {
 		mb_mailbox = UART_get_mailbox(UART_0);
 		if(mb_flag)
 		{
+			/**/
+			if(ASCII_ONE == mb_mailbox)
+			{
+				tera_get_hora();
+			}
+			else if(ASCII_TWO == mb_mailbox)
+			{
+				tera_set_hora();
+			}
+			else if(ASCII_ESC == mb_mailbox)
+			{
+				tera_menu();
+			}
+			/**/
+			/*For debug*/
+			/**Sends to the PCA the received data in the mailbox*/
+			//UART_put_char (UART_0, g_mail_box_uart_0.mailBox);
 
+			/**clear the reception flag*/
+			UART_empty_mailbox(UART_0);
 		}
-		UART_empty_mailbox(UART_0);
 	}
 
     return 0 ;
